1258cd1ebd9ada3fe631bfb9ab35a3574a82c26b
rhian-cs <rhian.luis.cs+github@gmail.com>
2024-03-15 09:46:03 -0300

    Pass text data from Ruby to Rust

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,6 +1,29 @@
+use std::ffi::{CStr, CString};
+
 #[no_mangle]
 pub extern "C" fn add(left: u32, right: u32) -> u32 {
     left + right
+}
+
+#[no_mangle]
+pub extern "C" fn process_request(raw_string_ptr: *const i8) -> *const i8 {
+    let request = unsafe { CStr::from_ptr(raw_string_ptr) }.to_str().unwrap();
+
+    let response = format!("You've requested: {request}");
+
+    let response = CString::new(response).unwrap();
+    response.into_raw()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn deallocate_ptr(ptr: *mut i8) {
+    if ptr.is_null() {
+        return;
+    }
+
+    unsafe {
+        let _ = CString::from_raw(ptr);
+    };
 }
 
 #[cfg(test)]
--- ruby/main.rb
+++ ruby/main.rb
@@ -4,7 +4,10 @@ module Adder
   extend FFI::Library
   ffi_lib '../adder/target/release/libadder.so'
   attach_function :add, [:uint32, :uint32], :uint32
+  attach_function :process_request, [:string], :strptr
+  attach_function :deallocate_ptr, [:pointer], :void
 end
 
-puts 1
-puts Adder.add(5, 3)
+result_str, result_ptr = Adder.process_request("a cup of coffee ☕")
+puts result_str
+Adder.deallocate_ptr(result_ptr)
